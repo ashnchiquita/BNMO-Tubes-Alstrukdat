3d8ad2213057d097a3fc12c147d79226d8acb30b
zidane <13521163@mahasiswa.itb.ac.id>
2022-10-29 00:34:53 +0700

    object union first commit

---
+++ adt/objectU.h
@@ -0,0 +1,11 @@
+#ifndef BNMO_TUBES_ALSTRUKDAT_UNIONU_H
+#define BNMO_TUBES_ALSTRUKDAT_UNIONU_H
+
+#include "makanan.h"
+
+typedef union objectU {
+    int intV;
+    Makanan makananV;
+} Object;
+
+#endif //BNMO_TUBES_ALSTRUKDAT_UNIONU_H
