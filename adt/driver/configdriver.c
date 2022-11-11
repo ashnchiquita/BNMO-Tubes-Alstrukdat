e2ea7e4048808768d1a38b2af331d88c2dbab1f8
hanifmz07 <13521157@std.stei.itb.ac.id>
2022-11-11 14:15:19 +0700

    Add driver config matrix/peta

--- adt/driver/configdriver.c
+++ adt/driver/configdriver.c
@@ -1,7 +1,12 @@
 #include "../config.h"
 
 int main(){
-    ListMakanan l= configMakanan();
-    printMakanan(l);
+    // ListMakanan l= configMakanan();
+    // printMakanan(l);
+
+    POINT lokasiPemain;
+    Matrix peta = configPeta(&lokasiPemain);
+    TulisPOINT(lokasiPemain); printf("\n");
+    displayPeta(peta); printf("\n");
     return 0;
 }
