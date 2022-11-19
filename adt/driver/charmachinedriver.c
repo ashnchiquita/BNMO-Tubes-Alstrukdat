d62d085efa94e3464d404a85728c23f178e4abc1
unknown <ferindyaulia19@gmail.com>
2022-11-19 21:15:28 +0700

    add driver charmachine and wordmachine

---
+++ adt/driver/charmachinedriver.c
@@ -0,0 +1,17 @@
+#include "../charmachine.h"
+#include <stdio.h>
+
+int main() {
+    char testFile[60] = "../../../pitakar.txt";
+    /* read char from file*/
+    STARTFILE(testFile);
+
+
+    // print char
+    while (!EOP) {
+        printf("%c", currentChar);
+        ADV();
+    }
+    printf(".\n");
+    return 0;
+}
